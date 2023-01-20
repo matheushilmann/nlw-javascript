cec271a65f7f8c8ab65ea7e4b31a4423368f0256
Matheus Hilmann Rodrigues <matheushilmann@gmail.com>
2023-01-20 10:17:53 -0300

    criando funções para adicionar o dia e armazenando ao local storage

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06'],
-  water: ['01-03', '01-04', '01-05']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert('Dia já incluso')
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('habitoDoDia', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('habitoDoDia')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
